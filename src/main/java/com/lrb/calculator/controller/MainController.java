f3beaf73734f83da6b147da13d21d230dc25a5a4
rockmaester <rockmaester.work@gmail.com>
2023-09-03 13:31:08 +0300

    ...

--- src/main/java/com/lrb/calculator/controller/MainController.java
+++ src/main/java/com/lrb/calculator/controller/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
 
+
     @GetMapping("")
     public String showGreetings(){
         return "greetings";
